00fe4d4b1dc6d59f285089c546002c624b8d4627
Ozgur OSMAN <osman.ozg@gmail.com>
2012-04-09 03:01:07 +0100

    new file:   index.php 	new file:   sections/page1.php 	new file:   sections/sidebar.php 	new file:   style.css

---
+++ sections/sidebar.php
@@ -0,0 +1,6 @@
+<p class="bookmark" style="padding-bottom:1em; font-weight:bold;">Jump menu</p>
+<a class="inside" href="#top"><p class="bookmark button">Acasa</p></a>
+<a class="inside" href="#q1"><p class="bookmark button">Question 1</p></a>
+<a class="inside" href="#q2"><p class="bookmark button">Question 2</p></a>
+<a class="inside" href="#q3"><p class="bookmark button">Question 3</p></a>
+<a class="inside" href="#outro"><p class="bookmark button">Final words</p></a>
