51bd9ce510d96dc49587e64fbdcdcd99e1766f46
Ozgur OSMAN <osman.ozg@gmail.com>
2012-04-10 01:14:32 +0100

    fonts

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 ﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml">
-    <head><link rel="stylesheet" href="style.css" type="text/css"></head>
+    <head>
+        <link rel="stylesheet" href="style.css" type="text/css">
+        <link href='http://fonts.googleapis.com/css?family=Dosis:200,300,400,500,600,700,800|Rosarivo:400,400italic|Pompiere|Quicksand:300,400,700|Ubuntu:300,400,500,700,300italic,400italic,500italic,700italic|Lobster|Averia+Serif+Libre:300,400,700,300italic,400italic,700italic|Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic|Fredoka+One|Copse|Asap:400,700,400italic,700italic|Raleway:100|Lobster+Two:400,400italic,700,700italic&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+    </head>
     <body><div id="blue"></div><div id="red"></div><div id="all">
         <div id="bookmarks"> 
             <?php include 'sections/sidebar.php'; ?>
